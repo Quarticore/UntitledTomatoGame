93c5a5231f8a7b3b552b41479273ffe4e0d459f8
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-08-30 22:59:18 -0700

    fix: cleanup

--- assets/scripts/metronome.gd
+++ assets/scripts/metronome.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @onready var Player: AudioStreamPlayer = $AudioStreamPlayer
-@onready var Output: Label = $Output
 @onready var Accuracy: Label = $Accuracy
 var input_within = 0.2
 var input_offset = -0.4
@@ -9,7 +8,6 @@ var bpm = 125.0
 var bps = bpm / 60
 var offset = 0.2
 var last_beat_elapsed = 0.0
-var paused_at = 0.0
 var combo = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -18,19 +16,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# When SPACE is RELEASED, play/pause
-	if Input.is_action_just_released("PauseSong_PLEASE_REMOVE"):
-		if !Player.playing:
-			print("playing")
-			Player.play(paused_at)
-		else:
-			print("pausing")
-			paused_at = Player.get_playback_position()
-			Player.stop()
-			
 	if !Player.playing:
 		return
-		
 	
 	var beats_elapsed: float = (Player.get_playback_position() / (1 / bps)) + offset
 	
@@ -39,7 +26,6 @@ func _process(delta: float) -> void:
 	# Round to whole number, see if different
 	if round(beats_elapsed) != round(last_beat_elapsed):
 		emit_signal("new_beat", beats_elapsed)
-		push_o_on_beat()
 	
 	last_beat_elapsed = beats_elapsed
 	
@@ -66,6 +52,4 @@ func check_input(beats_elapsed):
 	
 	combo = 0
 	emit_signal("rhythm_input", 0.0, combo)
-
-func push_o_on_beat():
-	Output.text = Output.text + "O"
+	emit_signal("combo_break")
