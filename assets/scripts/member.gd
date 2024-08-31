cec83a215e0653236215c4bc9bc0304c657015cb
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-08-31 01:53:33 -0700

    feat: dogshit member selection

--- assets/scripts/member.gd
+++ assets/scripts/member.gd
@@ -1,6 +1,13 @@
 extends Node
 
 signal hovered
+signal selected
 
 func _on_area_2d_mouse_entered() -> void:
 	hovered.emit()
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	# If clicked emit "selected"
+	if event.is_action("Click"):
+		selected.emit()
--- assets/scripts/selection_manager.gd
+++ assets/scripts/selection_manager.gd
@@ -4,6 +4,7 @@ extends Node
 @onready var Member1: Node2D = get_tree().root.get_node("Main/Member")
 @onready var Member2: Node2D = get_tree().root.get_node("Main/Member2")
 @onready var Member3: Node2D = get_tree().root.get_node("Main/Member3")
+@onready var Camera: Camera2D = get_tree().root.get_node("Main/MainCamera")
 
 func _on_member_hovered() -> void:
 	Light.global_position.x = Member1.global_position.x
@@ -13,3 +14,15 @@ func _on_member_2_hovered() -> void:
 
 func _on_member_3_hovered() -> void:
 	Light.global_position.x = Member3.global_position.x
+
+func _on_member_selected() -> void:
+	Camera.zoom = Vector2(2, 2)
+	Camera.global_position = Member1.global_position
+
+func _on_member_2_selected() -> void:
+	Camera.zoom = Vector2(2, 2)
+	Camera.global_position = Member2.global_position
+
+func _on_member_3_selected() -> void:
+	Camera.zoom = Vector2(2, 2)
+	Camera.global_position = Member3.global_position
