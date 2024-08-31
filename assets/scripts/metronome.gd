f26e6cf5b65ebddd37f3eacbabd7bcc6e07d485b
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-08-30 22:46:47 -0700

    feat: accuracy percentages

--- assets/scripts/metronome.gd
+++ assets/scripts/metronome.gd
@@ -2,11 +2,14 @@ extends Node
 
 @onready var Player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var Output: Label = $Output
-var paused_at = 0.0
+@onready var Accuracy: Label = $Accuracy
+var input_within = 0.3
+var input_offset = -0.3
 var bpm = 125.0
 var bps = bpm / 60
 var offset = 0.2
 var last_beat_elapsed = 0.0
+var paused_at = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,8 +29,11 @@ func _process(delta: float) -> void:
 			
 	if !Player.playing:
 		return
+		
 	
 	var beats_elapsed: float = (Player.get_playback_position() / (1 / bps)) + offset
+	
+	check_input(beats_elapsed + input_offset)
 	
 	# Round to whole number, see if different
 	if round(beats_elapsed) != round(last_beat_elapsed):
@@ -36,6 +42,20 @@ func _process(delta: float) -> void:
 	last_beat_elapsed = beats_elapsed
 	
 	pass
+	
+func check_input(beats_elapsed):
+	if !Input.is_action_just_pressed("Input_PLEASE_REMOVE"):
+		return
+	
+	var beat_whole = round(beats_elapsed)
+	var difference = abs(beats_elapsed - beat_whole)
+	
+	if (difference <= input_within):
+		# We hit! Show accuracy
+		var complement = input_within - difference
+		var as_percentage = (complement / input_within) * 100
+		Accuracy.text = str(as_percentage) + "%"
+		
 
 func push_o_on_beat():
 	Output.text = Output.text + "O"
