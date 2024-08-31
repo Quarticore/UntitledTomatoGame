189122519f31be5d612a840084a5d20ddd298926
Mazure3D <mazure3d@gmail.com>
2024-08-31 15:22:21 -0700

    Fix: ui elements instance paths

--- assets/scripts/menu.gd
+++ assets/scripts/menu.gd
@@ -1,11 +1,5 @@
 class_name Menu extends Control
 
 
-@onready var ui_viewport: SubViewport = %UIViewport
-
-
 func add_to_scene(scene: Node) -> void:
-	if ui_viewport:
-		ui_viewport.add_child(scene)
-	else:
 		get_tree().root.add_child(scene)
