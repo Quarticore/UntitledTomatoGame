58874651396f3c74de51ea7ba869d24e72f6d2e3
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-08-31 13:57:33 -0700

    fix: viewport debugger span

--- assets/scripts/camera_scale.gd
+++ assets/scripts/camera_scale.gd
@@ -1,9 +1,8 @@
 extends Camera2D
 
-@onready var tex: TextureRect = $ViewportTextureRect
+@onready var tex: Sprite2D = $ViewportTextureRect
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	tex.set_position(position, true)
 	tex.scale = Vector2(1, 1) / zoom
 	pass
