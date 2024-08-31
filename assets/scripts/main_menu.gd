cfbdfe98db5b4138139fccf9df78811bea5ad4bf
Mazure3D <mazure3d@gmail.com>
2024-08-30 23:32:39 -0700

    Add credits button

--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -5,5 +5,9 @@ func _on_play_button_pressed() -> void:
 	print("Load the game scene!")
 
 
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
+
+
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
