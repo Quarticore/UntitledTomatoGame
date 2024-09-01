fd55c41c7a056f1195678c2c809a866bcf004307
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-08-31 17:51:30 -0700

    fix: scale assets, use great and terrible

--- assets/scripts/gameplay_ui.gd
+++ assets/scripts/gameplay_ui.gd
@@ -2,6 +2,9 @@ extends Node
 
 const HIT_QUALITY_ITEM = preload("res://scenes/hit_quality_item.tscn")
 const HIT_PERFECT = preload("res://assets/images/input_perfect.png")
+const HIT_GREAT = preload("res://assets/images/input_great.png")
+const HIT_OKAY = preload("res://assets/images/input_okay.png")
+const HIT_MISS = preload("res://assets/images/input_terrible.png")
 
 func play_show():
 	$AnimationPlayer.play("fade_in")
@@ -10,22 +13,28 @@ func play_hide():
 	$AnimationPlayer.play("fade_out")
 
 func _on_audio_manager_rhythm_input(pct, combo, input) -> void:
+	var hit_asset
+	
 	if pct == 0.0:
 		print("Miss")
+		hit_asset = HIT_MISS
 	elif pct > 65:
 		print("Perfect")
+		hit_asset = HIT_PERFECT
 	elif pct > 35:
 		print("Great")
+		hit_asset = HIT_GREAT
 	else:
 		print("Okay")
+		hit_asset = HIT_OKAY
 	
 	print(pct)
 	
 	var rng = RandomNumberGenerator.new()
 	
 	var inst = HIT_QUALITY_ITEM.instantiate()
-	inst.texture = HIT_PERFECT
-	inst.scale = Vector2(0.5, 0.5)
+	inst.texture = hit_asset
+	inst.scale = Vector2(0.7, 0.7)
 	inst.rotation_degrees = rng.randi_range(5, 10)
 	
 	$HitQualityContainer.add_child(inst)
