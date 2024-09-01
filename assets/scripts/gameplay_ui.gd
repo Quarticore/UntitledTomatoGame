f184b8f66fb38bdb20a700e151b30a99e8271ccc
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-08-31 17:01:39 -0700

    feat: begin indicator sprites

--- assets/scripts/gameplay_ui.gd
+++ assets/scripts/gameplay_ui.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const HIT_QUALITY_ITEM = preload("res://scenes/hit_quality_item.tscn")
+const HIT_PERFECT = preload("res://assets/images/input_perfect.png")
+
 func play_show():
 	$AnimationPlayer.play("fade_in")
 
@@ -8,12 +11,20 @@ func play_hide():
 
 func _on_audio_manager_rhythm_input(pct, combo, input) -> void:
 	if pct == 0.0:
-		$HitQualityIndicator.text = "Miss"
+		print("Miss")
 	elif pct > 65:
-		$HitQualityIndicator.text = "Perfect!"
+		print("Perfect")
 	elif pct > 35:
-		$HitQualityIndicator.text = "Great!"
+		print("Great")
 	else:
-		$HitQualityIndicator.text = "Okay..."
-		
-	$HitQualityIndicator.text = $HitQualityIndicator.text + " (" + str(pct) + "%)"
+		print("Okay")
+	
+	print(pct)
+	
+	var inst = HIT_QUALITY_ITEM.instantiate()
+	inst.texture = HIT_PERFECT
+	inst.scale = Vector2(0.3, 0.3)
+	inst.rotation_degrees = 10.0
+	
+	$HitQualityContainer.add_child(inst)
+	
